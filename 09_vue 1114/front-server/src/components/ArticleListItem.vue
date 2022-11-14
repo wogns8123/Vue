64da177dbbc88837c980340476f30b4c32df7d7b
wogns8123 <Wogns8123@navr.com>
2022-11-14 11:36:28 +0900

    Vue

---
+++ 09_vue 1114/front-server/src/components/ArticleListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h5>{{article.id}}</h5>
+    <h5>{{article.title}}</h5>
+    
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleListItem',
+  props: {
+    article: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
